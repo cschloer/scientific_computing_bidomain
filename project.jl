8d4c864fbf408955313eccd33c261c124fda2f38
gregoirepourtier <gpourtier@icloud.com>
2021-03-15 22:47:44 +0100

    adding Dirichlet BCs (west) for the species 2

--- project.jl
+++ project.jl
@@ -51,8 +51,6 @@ $f(u, v)= u−\frac{u^{3}}{3}−v\;$
 
 $g(u, v)= u + \beta - \gamma v \;$
 
-$
-
 
 """
 
@@ -85,13 +83,6 @@ function evolution(inival, # initial value
 end
 
 
-# ╔═╡ a7724d46-76cb-11eb-2275-0b6db01e36b2
-# Define function for initial value $u_0$ with two methods - for 1D and 2D problems
-begin
-	fpeak(x)=exp(-100*(x-0.25)^2)
-	fpeak(x,y)=exp(-100*((x-0.25)^2+(y-0.25)^2))
-end
-
 # ╔═╡ 4b9f5030-76cc-11eb-117c-91ca8336c30b
 # Create discretization grid in 1D or 2D with approximately n nodes
 function create_grid(n,dim)
@@ -108,57 +99,63 @@ function create_grid(n,dim)
 end
 
 # ╔═╡ fa52bcd0-76f8-11eb-0d58-955a514a00b1
-function bidomain(;n=100,dim=1,sigma_i=1, sigma_e=1, epsilon=0.1, gamma=0.5, beta=1, tstep=0.05, tend=30,dtgrowth=1.0)
+function bidomain(;n=100,dim=1,sigma_i=1.0, sigma_e=1.0, epsilon=0.1, gamma=0.5, beta=1.0, tstep=0.05, tend=11,dtgrowth=1.0)
 
 	grid=create_grid(n,dim)
+	L=collect(0:70.0/n:70.0)
+	
 	function storage!(f,u,node)
         f.=u
     end
 
 	
-function bidomain_flux!(f,_u,edge)
+	function bidomain_flux!(f,_u,edge)
 		u=unknowns(edge,_u)
-    f[1] = sigma_i * (u[1,1] - u[1, 2]) + sigma_i * (u[2,1] - u[2,2])
-	f[2] = sigma_i * (u[1,1] - u[1, 2]) + (sigma_i + sigma_e) * (u[2,1]-u[2,2])
-	f[3] = 1
+		f[1] = sigma_i * (u[1,1] - u[1, 2]) + sigma_i * (u[2,1] - u[2,2])
+		f[2] = sigma_i * (u[1,1] - u[1, 2]) + (sigma_i + sigma_e) * (u[2,1]-u[2,2])
+		 f[3] = 1
 
-end
-# Reaction:
-function bidomain_reaction!(f,u,node)
-    f[1] = (1 / epsilon) *  (u[1]  + u[1] ^ 3 / 3 - u[3])
-    f[2] = 1
-	f[3] = epsilon * (u[1]  + beta - gamma * u[3])
-end
-	
-# Source
-function bidomain_source!(f,node)
-	f[1] = 1
-	f[2] = 1
-	f[3] = 1
-end
-	
-# Create system
-bidomain_physics=VoronoiFVM.Physics(flux=bidomain_flux!,storage=storage!,
-                                 num_species=3,reaction=bidomain_reaction!, 										 source=bidomain_source!)
-bidomain_system=VoronoiFVM.DenseSystem(grid,bidomain_physics)
-enable_species!(bidomain_system,1,[1])
-enable_species!(bidomain_system,2,[1])
-enable_species!(bidomain_system,3,[1])
-
-	inival=unknowns(bidomain_system)
-for i=1:num_nodes(grid)
-	if i < num_nodes(grid) / 20
-    	inival[1,i]= 2
-	else
-		inival[1,i]= 0 
 	end
-			
-    inival[2,i]=0
-	inival[3,i]=0
+	
+	# Reaction:
+	function bidomain_reaction!(f,u,node)
+		f[1] = (1 / epsilon) *  (u[1]  + ((u[1] ^ 3) / 3) - u[3])
+		f[2] = 1
+		f[3] = epsilon * (u[1]  + beta - gamma * u[3])
+	end
 
-end
+	# Source
+	function bidomain_source!(f,node)
+		f[1] = 1
+		f[2] = 1
+		f[3] = 1
+	end
+
+	# Create system
+	bidomain_physics=VoronoiFVM.Physics(flux=bidomain_flux!,storage=storage!,
+									 num_species=3,reaction=bidomain_reaction!, 										 source=bidomain_source!)
+	bidomain_system=VoronoiFVM.DenseSystem(grid,bidomain_physics)
+	enable_species!(bidomain_system,1,[1])
+	enable_species!(bidomain_system,2,[1])
+	enable_species!(bidomain_system,3,[1])
+
+	boundary_dirichlet!(bidomain_system, 2, 1 ,0)
+
+	inival=unknowns(bidomain_system, inival=0)
+	for i=1:num_nodes(grid)
+		if L[i] < 70.0 / 20
+			inival[1,i]= 2
+		else
+			inival[1,i]= 0 
+		end
+
+		inival[2,i]=0
+		inival[3,i]=0
+
+	end
 
 	evolution(inival,bidomain_system,grid,tstep,tend,dtgrowth)	
+	
 end
 
 
@@ -176,19 +173,16 @@ let
 	scalarplot!(bivis[1,1],result_bidomain.grid,
 	       result_bidomain.solutions[t_bidomain][1,:],
 		   title="u: t=$(round(result_bidomain.times[t_bidomain], digits=2))",
-	       flimits=(0,3),colormap=:cool,levels=50,clear=true)
+	       flimits=(-2,2),colormap=:cool,levels=50,clear=true)
 	scalarplot!(bivis[1,2],result_bidomain.grid,
 	       result_bidomain.solutions[t_bidomain][2,:],
 		   title="u_e: t=$(round(result_bidomain.times[t_bidomain], digits=2))",
-	       flimits=(0,3),colormap=:cool,levels=50,show=true)
+	       flimits=(-2,2),colormap=:cool,levels=50,show=true)
 	scalarplot!(bivis[1,3],result_bidomain.grid,
 	       result_bidomain.solutions[t_bidomain][3,:],
 		   title="v: t=$(round(result_bidomain.times[t_bidomain], digits=2))",
-	       flimits=(0,3),colormap=:cool,levels=50,show=true)
+	       flimits=(-2,2),colormap=:cool,levels=50,show=true)
 end
-
-# ╔═╡ 19d6cc30-85af-11eb-3e69-ffc5f9b28f73
-
 
 # ╔═╡ 3ab28264-6c64-11eb-29f4-a9ed2e9eba16
 TableOfContents()
@@ -201,14 +195,12 @@ end
 # ╔═╡ Cell order:
 # ╠═60941eaa-1aea-11eb-1277-97b991548781
 # ╟─48b1a0ac-76f3-11eb-05bd-cbcfae8e2f27
-# ╠═397c9290-76f5-11eb-1114-4bd31f7ecf9a
+# ╟─397c9290-76f5-11eb-1114-4bd31f7ecf9a
 # ╠═633b3d12-76a4-11eb-0bc7-b9bf9116933f
-# ╠═a7724d46-76cb-11eb-2275-0b6db01e36b2
 # ╠═4b9f5030-76cc-11eb-117c-91ca8336c30b
 # ╠═fa52bcd0-76f8-11eb-0d58-955a514a00b1
 # ╠═4e66a016-76f9-11eb-2023-6dfc3374c066
 # ╠═106d3bc0-76fa-11eb-1ee6-3fa73be52226
 # ╠═e2cbc0ec-76f9-11eb-2870-f10f6cdc8be4
-# ╠═19d6cc30-85af-11eb-3e69-ffc5f9b28f73
 # ╟─3ab28264-6c64-11eb-29f4-a9ed2e9eba16
 # ╟─d32173ec-66e8-11eb-11ad-f9605b4964b2
